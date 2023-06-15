fc3c77b0e21f335b3bdad4fc80d8b0884098167f
Grant Hall <grantclone@gmail.com>
2023-06-15 15:31:04 +0900

    add auto player

--- youtube-search.rb
+++ youtube-search.rb
@@ -3,25 +3,30 @@ require 'open-uri'
 require 'json'
 
 def search(query)
-  query = query.gsub(' ', '\\ ')
-  index = 1
-  api_key = ENV['API_KEY'] # NOT WORKING REPACE ME
+  unless query = query.gsub(' ', '\\ ')
+    return puts 'ERROR: invalid'
+  end
 
-  # from postman using youtube data api key
+  # get top 10 results from youtube
+  api_key = ENV[API_KEY]
   response = URI.parse("https://www.googleapis.com/youtube/v3/search?key=#{api_key}&q=#{query}&type=video&part=snippet").read
   data = JSON.parse(response)
 
   50.times { print '-' }
 
+  index = 1
+  titles = []
   urls = []
 
   data['items'].each do |video|
+    # define name, desc, url for each video and display it
     title = video['snippet']['title']
     description = "  -  #{video['snippet']['description']}"
     url = "https://www.youtube.com/watch?v=#{video['id']['videoId']}"
     urls << url
+    titles << title
 
-    puts "\n[#{index}] - #{title} \n#{description} \n\n #{url}"
+    puts "\n[#{index}] - #{title} \n#{description}\n"
     index += 1
     50.times { print '-' }
   end
@@ -29,5 +34,5 @@ def search(query)
   puts "\n\nCHOOSE A NUMBER:"
   index = STDIN.getc.to_i - 1
 
-  `yt-dlp -o '~/Downloads/%(title)s.%(ext)s' #{urls[index]}`
+  `yt-dlp -o '~/Downloads/%(title)s.%(ext)s' #{urls[index]} && mplayer ~/Downloads/#{titles[index].gsub(' ', '\\ ')}.webm`
 end
